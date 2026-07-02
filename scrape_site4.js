4bbb493918c0f78b398e06000efb6c2046ac374f
Mokax1 <shadowwalker141992@gmail.com>
2026-07-02 11:23:04 +0300

    Update scrape_site4.js

--- scrape_site4.js
+++ scrape_site4.js
@@ -6,6 +6,7 @@ const xlsx = require('xlsx');
     const browser = await chromium.launch({ headless: true });
     const context = await browser.newContext({
         userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36'
+        timezoneId: 'Europe/Paris'
     });
     const page = await context.newPage();
 
